68973e3dc61243b99fadd337ed66b634d2b11d23
Jeanette Huang <jeanette.y.huang@gmail.com>
2012-04-04 23:20:45 -0500

    Made it so that only available meeting times are shown. (No longer displaying entries where numberbusy != 0)

--- calendar2/app/controllers/free_times_controller.rb
+++ calendar2/app/controllers/free_times_controller.rb
@@ -1,10 +1,11 @@
 class FreeTimesController < ApplicationController
   def index
-    @hours = Hour.all
-
+    @hours = Hour.find_all_by_numberbusy(0)
+  #  @free_times = Hour.find_all_by_numberbusy(0)
+    
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @hours }
+     format.html # index.html.erb
+     format.json { render json: @hours } 
     end
   end
 end
